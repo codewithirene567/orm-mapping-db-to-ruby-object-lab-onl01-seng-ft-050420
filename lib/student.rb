67f00c13bf74546c47524872781c4a7840578f85
Irene Scott <irenejinscott@gmail.com>
2020-06-04 23:56:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,16 @@ class Student
   end.first
   end
 
-  def self.
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.create_table
