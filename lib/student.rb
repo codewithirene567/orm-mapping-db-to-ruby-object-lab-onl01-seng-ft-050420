a32d685c4e292a9aa8a042e5da7d872b440982db
Irene Scott <irenejinscott@gmail.com>
2020-06-04 23:41:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,18 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
+  def self.all_students_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade = 10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
