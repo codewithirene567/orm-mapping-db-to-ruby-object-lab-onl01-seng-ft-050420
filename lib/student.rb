e68d894589f46bad62a739dd30f97594f4eaa04a
Irene Scott <irenejinscott@gmail.com>
2020-06-04 23:41:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,16 +59,17 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
-  def self.all_students_in_grade_10
+  def self.first_student_in_grade_10
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE students.grade = 10
+    LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
-    end
+  end.first
   end
 
   def self.create_table
