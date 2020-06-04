e2f2e210e994e04bcaf15d76dfca99fa4de8636d
Irene Scott <irenejinscott@gmail.com>
2020-06-04 23:23:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,14 @@ class Student
      end.first
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    LIMIT 1
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
