dc298231471b84918b41bed887dcf61de0e669c2
Irene Scott <irenejinscott@gmail.com>
2020-06-05 00:10:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,6 +97,19 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE students.grade = ?
+    LIMIT ?
+    SQL
+
+    students = DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
